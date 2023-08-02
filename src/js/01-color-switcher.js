a4f0181ed2fe2588668aae9776de5679c17a5a5f
Sergey Opasen <ykcyc13@gmail.com>
2023-08-02 21:27:51 +0200

    hw-09

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,21 @@
+
+const changeColorBtn = document.querySelector('[data-start]');
+const stopChangeColorBtn = document.querySelector('[data-stop]');
+changeColorBtn.addEventListener('click', changeColor);
+stopChangeColorBtn.addEventListener('click', stopChangeColor);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+function changeColor() {
+    changeColorBtn.disabled = true;
+    stopChangeColorBtn.disabled = false;
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+function stopChangeColor() {
+    changeColorBtn.disabled = false;
+    stopChangeColorBtn.disabled = true;
+    clearInterval(intervalId);
+}
+
