fe919819504d0e4dba49e680122b2253719b612c
CepreuOnaceH <127330618+CepreuOnaceH@users.noreply.github.com>
2023-08-02 19:28:11 +0000

    Deploying to gh-pages from @ CepreuOnaceH/goit-js-hw-09@a4f0181ed2fe2588668aae9776de5679c17a5a5f 🚀

---
+++ 01-color-switcher.34c2a0d1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,intervalId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(intervalId)}));
+//# sourceMappingURL=01-color-switcher.34c2a0d1.js.map
