2c38caccfe4d18a041bb1a7864421a1c2c5f12f0
Sergey Opasen <ykcyc13@gmail.com>
2023-08-05 16:54:03 +0200

    hw-09

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
+import Notiflix from 'notiflix';
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
   }
-}
+
+  document.querySelector(".form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const delay = parseInt(formData.get("delay"));
+    const step = parseInt(formData.get("step"));
+    const amount = parseInt(formData.get("amount"));
+
+    for (let i = 0; i < amount; i+=1) {
+      createPromise(i + 1, delay + step * i)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms.`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms.`);
+        });
+    }
+  });
