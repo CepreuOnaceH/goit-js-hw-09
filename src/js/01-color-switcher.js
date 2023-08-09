c1cd4c2776d3ddf0d153c35b3a1e7b6753e473f6
Sergey Opasen <ykcyc13@gmail.com>
2023-08-09 20:31:47 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,15 +2,12 @@ const changeColorBtn = document.querySelector('[data-start]');
 const stopChangeColorBtn = document.querySelector('[data-stop]');
 changeColorBtn.addEventListener('click', changeColor);
 stopChangeColorBtn.addEventListener('click', stopChangeColor);
-// let intervalId = ();
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-let intervalId = setInterval(() => {
-  document.body.style.backgroundColor = getRandomHexColor();
-}, 1000);
+let intervalId = null;
 function changeColor() {
   changeColorBtn.disabled = true;
   stopChangeColorBtn.disabled = false;
